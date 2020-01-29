211c11fa7a1749b06de76e0b97982d3aa8da1318
anton.korniichuk <anton.korniichuk@xenial.com>
2020-01-29 07:28:19 +0200

    Quote updated

--- src/types/quote.type.ts
+++ src/types/quote.type.ts
@@ -2,7 +2,17 @@ import { IDInt, ISODate } from './ts-alias.type';
 
 export interface Quote {
   id: IDInt;
+  /**
+   * The text of the quote
+   */
   text: number;
+  /**
+   * Optional quote description
+   */
+  description?: string;
+  /**
+   * Owner of the quote
+   */
   userId: IDInt;
   authorIds: IDInt[];
   tagIds: IDInt[];
